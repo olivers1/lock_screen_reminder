92ea9cd54fbbf0ade9b07b9b8486e2bad5374f3b
Oliver Staberg <oliver.staberg@gmail.com>
2020-01-02 16:22:03 +0100

    Add project files.

---
+++ lock_screen_reminder.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		lock_screen_reminder.ino
+ Created:	1/2/2020 4:19:35 PM
+ Author:	olivers
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
